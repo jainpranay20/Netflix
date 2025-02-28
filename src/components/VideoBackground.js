3896c2fa07504547e41274e3de87a38a56de350f
Pranay Jain <jainpranay20@gmail.com>
2025-02-28 14:38:49 +0530

    Creating custom hooks

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,23 +1,11 @@
-import React, { useEffect } from 'react'
-import { API_OPTIONS } from '../utils/constants'
-import { useDispatch, useSelector } from 'react-redux'
-import { addTrailerVideo } from '../utils/moviesSlice';
+import React from 'react'
+import useMovieTrailer from '../hooks/useMovieTrailer';
+import { useSelector } from 'react-redux';
 
 const VideoBackground = ({ movieId }) => {
-    const dispatch = useDispatch();
-    const trailerVideo =useSelector(store => store.movies?.trailerVideo); 
-    console.log(trailerVideo, 'trailerVideo')
-    const getMovieVideos = async () => {
-        const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, API_OPTIONS)
-        const json = await data.json();
-        const filterData = json.results.filter((item) => item.type === 'Trailer')
-        const trailer = filterData.length ? filterData[0] : json.results[0];
-        dispatch(addTrailerVideo(trailer));
-    }
+    useMovieTrailer(movieId);
+    const trailerVideo = useSelector(store => store.movies?.trailerVideo); 
 
-    useEffect(() => {
-        getMovieVideos();
-    }, [])
     return (
         <div>
             <iframe
